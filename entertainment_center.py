26508689d461081a794b1475ad71e23393200c08
sid9194 <sid9194@gmail.com>
2016-08-04 16:03:49 -0700

    Corrected
    Coding standards and comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,15 @@
 import fresh_tomatoes
 import media
 
-pursuit_of_happiness=media.Movie("Pursuit Of Hapiness",
-                                 "Life struggle to happiness",
-                                 "https://upload.wikimedia.org/wikipedia/en/8/81/Poster-pursuithappyness.jpg",
-                                 "https://www.youtube.com/watch?v=89Kq8SDyvfg")
-                                 
-fantastic_fox=media.Movie("fantastic Mr Fox",
-                         "Fox story",
-                         "https://upload.wikimedia.org/wikipedia/en/a/af/Fantastic_mr_fox.jpg",
-                         "https://www.youtube.com/watch?v=n2igjYFojUo")
-zootopia=media.Movie("zootopia",
+pursuit_of_happiness = media.Movie("Pursuit Of Hapiness",
+                                   "Life struggle to happiness",
+                                   "https://upload.wikimedia.org/wikipedia/en/8/81/Poster-pursuithappyness.jpg",
+                                   "https://www.youtube.com/watch?v=89Kq8SDyvfg")
+fantastic_fox = media.Movie("fantastic Mr Fox",
+                            "Fox story",
+                            "https://upload.wikimedia.org/wikipedia/en/a/af/Fantastic_mr_fox.jpg",
+                            "https://www.youtube.com/watch?v=n2igjYFojUo")
+zootopia = media.Movie("zootopia",
                      "Bunny police officer",
                      "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
                      "https://www.youtube.com/watch?v=jWM0ct-OLsM")
@@ -23,11 +22,12 @@ avatar = media.Movie("Avatar",
                      "A marine goes to alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY" )
-jason_bourne=media.Movie("Jason Bourne",
+jason_bourne = media.Movie("Jason Bourne",
                          "FBI officer on run",
                          "https://upload.wikimedia.org/wikipedia/en/b/b2/Jason_Bourne_%28film%29.jpg",
                          "https://www.youtube.com/watch?v=MIJ53m77-kw")
-
+#creating list of movies datastructure
 movies=[jason_bourne,pursuit_of_happiness,fantastic_fox,zootopia,toy_story,avatar]
+#calling open movie page function with movies as parameter
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+""" This class stores movie information """
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, 
                  trailer_youtube):
